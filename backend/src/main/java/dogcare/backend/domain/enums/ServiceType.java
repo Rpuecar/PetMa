e1d70656636fcb32cfac20a01ba20346976b05b0
rpuerto <raqpuecar@gmail.com>
2026-05-26 18:09:30 +0200

    Añadidos los enums al domain

---
+++ backend/src/main/java/dogcare/backend/domain/enums/ServiceType.java
@@ -0,0 +1,3 @@
+package com.dogcare.backend.domain.enums;
+
+public enum ServiceType {GUARDERIA, RESIDENCIA, PASEO, MIXTO}
---
+++ backend/src/main/java/dogcare/backend/domain/enums/WorkerSpecialty.java
@@ -0,0 +1,3 @@
+package com.dogcare.backend.domain.enums;
+
+public enum WorkerSpecialty {CUIDADOR, PASEADOR, AMBOS}
